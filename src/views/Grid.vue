c21373aeb44d4c65d10920949bfc2ec4bae87851
Fernando Gonzalez <fernandojaviergonzalez2018@gmail.com>
2022-01-22 13:36:42 -0300

    bootstrap vue

---
+++ src/views/Grid.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mt-5">
+        <b-row>
+            <b-col cols="12" md="6" lg="6">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis iure laudantium, maxime quo enim odio, id ullam laboriosam aliquid natus soluta voluptates dicta esse facilis illum, assumenda animi explicabo quasi.</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="6">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis iure laudantium, maxime quo enim odio, id ullam laboriosam aliquid natus soluta voluptates dicta esse facilis illum, assumenda animi explicabo quasi.</p>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols="12" md="6" lg="6">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis iure laudantium, maxime quo enim odio, id ullam laboriosam aliquid natus soluta voluptates dicta esse facilis illum, assumenda animi explicabo quasi.</p>
+            </b-col>
+            <b-col cols="12" md="6" lg="6">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis iure laudantium, maxime quo enim odio, id ullam laboriosam aliquid natus soluta voluptates dicta esse facilis illum, assumenda animi explicabo quasi.</p>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Grid'
+}
+</script>
